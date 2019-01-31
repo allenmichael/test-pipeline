f506b2be577494505c80e6fa5fd9dce62ded73bb
AM Grobelny <allenmichaelgrobelny@gmail.com>
2019-01-30 23:17:05 -0800

    ddb

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    region: process.env.AWS_REGION,
+    tbl: process.env.SIGNUP_TABLE
+  });
 });
 
 module.exports = router;
